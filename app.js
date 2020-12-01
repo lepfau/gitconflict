9f6630a297e9965c8d32a3343751794edb06c94e
Kate Pavlova <ekaterina.vik.pavlova@gmail.com>
2020-12-01 19:06:00 +0400

    with routing

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require ("express")
 const app = express()
+const hbs = require("hbs")
 
-app.listen(4000, ()=> {
-    console.log("server is ready http://localhost:4000")
-})
+
+
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/views/index.html");
+  });
+  app.get("/about", (request, response) => {
+    response.sendFile(__dirname + "/views/about.html");
+  });
+  app.get("/works", (request, response) => {
+    response.sendFile(__dirname + "/views/works.html");
+  });
+
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:3000`)
+  })
